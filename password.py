2e96e31332c0d024b3d882e3e7fc6aff447af7b9
Weishow <tino28082000@gmail.com>
2020-10-16 19:34:26 +0800

    Add new file

--- password.py
+++ password.py
@@ -1,19 +1,18 @@
 #密碼重試程式
-x = 2
-while True:
-	password = input("請輸入密碼")
+x = 3
+password = "a123456"
+while x > 0:
+	pwd = input("請輸入密碼")
 
-	if password == "a123456" :
-		print("登入成功")
+	if pwd == password :
+		print("登入成功!")
 		break
 
 	else :
-		print("你還有" , x , "次機會" )
-		
 		x = x - 1
-
-	if x < 0 :
-		break
+		print("登入失敗，你還有" , x , "次機會" )
+	
+	
 
 	
 
