929c30490ba12bb684988d71dd6c40ac6e29d745
Weishow <tino28082000@gmail.com>
2020-10-19 15:40:56 +0800

    Add new file

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,20 @@
 import random
+l = input("請輸入猜數字下限: ")
+h = input("請輸數猜數字上限: ")
+count = 0
+l = int(l)
+h = int(h)
 
-r = random.randint(1, 100)
+
+r = random.randint(l, h)
 
 while True :
 	num = input("請猜數字：")
 	num = int(num)
+	count += 1
 	if num == r :
 			print("恭喜你猜對了！")
+			print("你最後猜了", count, "次！" )
 			break
 
 	elif num > r :
@@ -14,3 +22,6 @@ while True :
 
 	else :
 			print("再高一點")
+
+	print("你已經猜了", count, "次了！" )		
+
